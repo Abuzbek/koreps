e33b371fceae732609a08231eab02107ad030f64
Abuzbek <68583672+Abuzbek@users.noreply.github.com>
2021-08-17 20:43:01 +0500

    update

--- components/app/Drawer.vue
+++ components/app/Drawer.vue
@@ -16,23 +16,47 @@
           <v-list-item-title v-text="item.title" />
         </v-list-item-content>
       </v-list-item>
+      <v-list-item @click="installPWA" exact style="color:#282846 !important;">
+        <v-list-item-action>
+          <v-icon>mdi-file-download</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="'Install'" />
+        </v-list-item-content>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 <script>
 export default {
-  props:{
-    drawer:{
-      type:Boolean,
+  data: () => ({
+    installable: false,
+    deferredPrompt: null
+  }),
+  props: {
+    drawer: {
+      type: Boolean
     },
-    items:{
-      type:Array,
+    items: {
+      type: Array
     }
   },
-  // computed:{
-  //   drawerEps: function(){
-  //       return this.drawer
-  //     }
-  // }
-}
+  mounted() {
+    window.addEventListener("beforeinstallprompt", e => {
+      console.log(e);
+      e.preventDefault();
+      this.deferredPrompt = e;
+    });
+
+    window.addEventListener("appinstalled", () => {
+      this.deferredPrompt = null;
+    });
+  },
+  methods: {
+    async installPWA() {
+      console.log(this.deferredPrompt);
+      this.deferredPrompt.prompt();
+    }
+  }
+};
 </script>
